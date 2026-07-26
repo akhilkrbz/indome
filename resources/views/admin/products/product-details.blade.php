c966258df9b853cad2d667d9b8300c605f637704
Akhil <akhilk1904@gmail.com>
2026-07-26 19:59:16 +0530

    Breadcrumbp update

--- resources/views/admin/products/product-details.blade.php
+++ resources/views/admin/products/product-details.blade.php
@@ -5,15 +5,7 @@
     <div class="page-wrapper">
         <div class="page-content">
             <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-                <div class="breadcrumb-title pe-3">eCommerce</div>
-                <div class="ps-3">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb mb-0 p-0">
-                            <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Product Details</li>
-                        </ol>
-                    </nav>
-                </div>
+                @include('admin.layouts.breadcrumb')
             </div>
 
             <div class="card">
