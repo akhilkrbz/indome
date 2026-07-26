6d59db1e81f5702c6c12e6755d1c50db432f108d
Akhil <akhilk1904@gmail.com>
2026-07-26 19:41:04 +0530

    Updates

--- resources/views/admin/products/add-variants.blade.php
+++ resources/views/admin/products/add-variants.blade.php
@@ -9,37 +9,13 @@
 
             <!--breadcrumb-->
             <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-                <div class="breadcrumb-title pe-3">eCommerce</div>
-                <div class="ps-3">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb mb-0 p-0">
-                            <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
-                            </li>
-                            <li class="breadcrumb-item active" aria-current="page">Orders</li>
-                        </ol>
-                    </nav>
-                </div>
-                <div class="ms-auto">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-primary">Settings</button>
-                        <button type="button" class="btn btn-primary split-bg-primary dropdown-toggle dropdown-toggle-split"
-                            data-bs-toggle="dropdown"> <span class="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-end"> <a class="dropdown-item"
-                                href="javascript:;">Action</a>
-                            <a class="dropdown-item" href="javascript:;">Another action</a>
-                            <a class="dropdown-item" href="javascript:;">Something else here</a>
-                            <div class="dropdown-divider"></div> <a class="dropdown-item" href="javascript:;">Separated
-                                link</a>
-                        </div>
-                    </div>
-                </div>
+                @include('admin.layouts.breadcrumb')
             </div>
             <!--end breadcrumb-->
 
             <div class="card">
                 <div class="card-body p-4">
-                    <h5 class="card-title">Add New Product</h5>
+                    <h5 class="card-title">Add New Product Variant</h5>
                     <hr />
                     @include('admin.layouts.alerts')
                     <div class="form-body mt-4">
--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -30,9 +30,9 @@
                             <div class="d-flex align-items-center mt-3 fs-6">
                                 <div class="cursor-pointer">
                                     <a href="{{ route('products.edit', $item->id) }}" class="btn btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Edit Product"><i class='bx bxs-edit text-warning'></i></a>
-                                    <button type="button" class="btn btn-sm"  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Remove"><i class='bx bxs-trash text-danger'></i></button>
+                                    <!-- <button type="button" class="btn btn-sm"  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Remove"><i class='bx bxs-trash text-danger'></i></button> -->
                                     <button type="button" class="btn btn-sm"  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Add Images"><i class='bx bxs-image-add text-primary'></i></button>
-                                    <a href="{{ route('products.add.variants', $item->id) }}"><button type="button" class="btn btn-sm"  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Add Variants"><i class='bx bxs-book-add text-dark'></i></button></a>
+                                    <!-- <a href="{{ route('products.add.variants', $item->id) }}"><button type="button" class="btn btn-sm"  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Add Variants"><i class='bx bxs-book-add text-dark'></i></button></a> -->
                                     <a href="{{ route('products.variants.list', $item->id) }}"><button type="button" class="btn btn-sm"  data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="View Variants"><i class='bx bxs-package text-success'></i></button></a>
                                 </div>
                                 <!-- <p class="mb-0 ms-auto">4.2(182)</p> -->
